1da13d6bf6cd89e4f8831f3314fc5d9c29108fd3
pkumars397 <pkumars397@gmail.com>
2023-09-15 18:39:35 +0530

    done the dom assignment 1

---
+++ DOM/js/01.js
@@ -0,0 +1,12 @@
+// console.log(document);
+// console.log(document.all);
+// console.log(document.all[0])
+// console.log(document.getElementById("header-title").textContent)
+// console.log(document.getElementById("header-title").innerText)
+// console.log(document.getElementById("header-title").innerHTML = "<p>Hello ji </p>");
+
+const heading = document.getElementById("main-header");
+
+heading.style.borderBottom = "5px solid black";
+
+document.getElementsByClassName("title")[0].style.color="green"

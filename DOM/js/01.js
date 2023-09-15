d55c7b607910613a59e333995fc8f44ae634ca27
pkumars397 <pkumars397@gmail.com>
2023-09-15 22:27:39 +0530

    get element by tagname task completed

--- DOM/js/01.js
+++ DOM/js/01.js
@@ -4,19 +4,25 @@
 // console.log(document.getElementById("header-title").textContent)
 // console.log(document.getElementById("header-title").innerText)
 // console.log(document.getElementById("header-title").innerHTML = "<p>Hello ji </p>");
+// ! Part 1 Get element by id
+// const heading = document.getElementById("main-header");
 
-const heading = document.getElementById("main-header");
+// heading.style.borderBottom = "5px solid black";
 
-heading.style.borderBottom = "5px solid black";
+// document.getElementsByClassName("title")[0].style.color = "green";
 
-document.getElementsByClassName("title")[0].style.color = "green";
+// ! Part2 get element by classname(give array HTMLCollection)
+// const items = document.getElementsByClassName("list-group-item");
+// console.log(items);
+// console.log(items[0]);
 
-// ! Part2
-const items = document.getElementsByClassName("list-group-item");
-console.log(items);
-console.log(items[0]);
-
-items[2].style.backgroundColor = "green"
-for (let val of items) {
-    val.style.fontWeight="bold"
+// items[2].style.backgroundColor = "green"
+// for (let val of items) {
+//     val.style.fontWeight="bold"
+// }
+// ! Part 3 Get element by TagName()(its also gives HTMLCollection)
+const li = document.getElementsByTagName("li");
+console.log(li);
+for (let i = 0; i < li.length; i++){
+    li[i].style.backgroundColor = "#474747";
 }

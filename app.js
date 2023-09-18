fcda0e8618d7c96afcdcf5684fbe265dce2fb90d
pkumars397 <pkumars397@gmail.com>
2023-09-18 23:33:19 +0530

    stored the form data into localStorage

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 let form = document.getElementById("form");
-console.log(form);
-form.addEventListener(("submit"), (event) => {
+// console.log(form);
+// form.addEventListener(("submit"), (event) => {
+//     event.preventDefault();
+//     console.log(event.target.fullName.value);
+//     console.log(event.target.emailId.value)
+// })
+
+form.addEventListener("submit", (event) => {
     event.preventDefault();
-    console.log(event.target.fullName.value);
-    console.log(event.target.emailId.value)
+    localStorage.setItem("Full Name",event.target.fullName.value)
+    localStorage.setItem("EmailId:",event.target.emailId.value)
+    localStorage.setItem("Phone:",event.target.phone.value)
+    localStorage.setItem("Time to call", `${event.target.date.value} ${event.target.time.value}`);
 })
-
-

19ab435b75ed3280ef1a5ccf3d62cb04ce0b0f76
pkumars397 <pkumars397@gmail.com>
2023-09-19 00:53:04 +0530

    added object into localStorage

--- app.js
+++ app.js
@@ -6,10 +6,26 @@ let form = document.getElementById("form");
 //     console.log(event.target.emailId.value)
 // })
 
+// form.addEventListener("submit", (event) => {
+//     event.preventDefault();
+//     localStorage.setItem("Full Name",event.target.fullName.value)
+//     localStorage.setItem("EmailId:",event.target.emailId.value)
+//     localStorage.setItem("Phone:",event.target.phone.value)
+//     localStorage.setItem("Time to call", `${event.target.date.value} ${event.target.time.value}`);
+// })
+
 form.addEventListener("submit", (event) => {
     event.preventDefault();
-    localStorage.setItem("Full Name",event.target.fullName.value)
-    localStorage.setItem("EmailId:",event.target.emailId.value)
-    localStorage.setItem("Phone:",event.target.phone.value)
-    localStorage.setItem("Time to call", `${event.target.date.value} ${event.target.time.value}`);
-})
+    let details = {
+        Name: event.target.fullName.value,
+        Email: event.target.emailId.value,
+        Phone: event.target.phone.value,
+        Time:event.target.date.value+" "+event.target.time.value
+    };
+
+    let details_stringfy = JSON.stringify(details);
+    localStorage.setItem("userDetails", details_stringfy)
+    let details_destringfy = JSON.parse(localStorage.getItem("userDetails"));
+    console.log(details_destringfy)
+});
+
